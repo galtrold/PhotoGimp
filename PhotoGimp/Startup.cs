9266c897bb0ae2d4236c976a58582971ab21bbd9
James Hansen <james@galtrold.dk>
2016-05-19 07:25:31 +0200

    First commit

---
+++ PhotoGimp/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhotoGimp.Repositories;
+using PhotoGimp.Services;
+
+namespace PhotoGimp
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            Config = builder.Build();
+        }
+
+        public IConfigurationRoot Config { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<PhotoGimpDbContext>(options => options.UseSqlServer(Config["database:connectionString"]));
+            
+            services.AddInstance<IConfiguration>(Config);
+            services.AddScoped<PhotoRepository, PhotoRepository>();
+            services.AddScoped<MediaService, MediaService>();
+            services.AddMvc();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvcWithDefaultRoute();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
